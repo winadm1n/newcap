86347c04be04e96472db57ffbf370411f05e0ccc
KVNP <kvnp179@gmail.com>
2023-05-02 22:36:08 +0530

    capitalizables set and get

--- src/Components/ToggleSwitch.js
+++ src/Components/ToggleSwitch.js
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactSwitch from 'react-switch';
 
-function ToggleSwitch() {
+function ToggleSwitch({capValue,epicStoryName}) {
   const [checked, setChecked] = useState(true);
 
   const handleChange = val => {
+    console.log("value for ",epicStoryName,"is being changed from",checked,"to",!checked)
+    if(checked){
+        setCapitalizables(epicStoryName,0)
+    } else {
+        setCapitalizables(epicStoryName,1)
+    }
     setChecked(val)
+
   }
+
+  const setInitialValue = ()=>{
+    if(capValue==="Capitalized") 
+    setChecked(true)
+    else
+    setChecked(false)
+  }
+
+  const setCapitalizables = async (EpicStoryName,value) => {
+    console.log("setting cap",value," for epic",EpicStoryName)
+    const requestOptions = { method: 'GET' }
+    await fetch(`http://localhost:5003/capitalize?EpicStoryName=${EpicStoryName}&value=${value}`, requestOptions)
+      .catch((error) => console.log('error', error))
+  };
+
+  useEffect(() => {
+    console.log("got cap ",capValue,"for epic button", epicStoryName);
+    setInitialValue()
+  }, []);
 
   return (
       <ReactSwitch
